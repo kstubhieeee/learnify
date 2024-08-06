c5522635f513276c8539682ea7f1969788b02766
kstubhie <banekaustubh27@gmail.com>
2024-08-07 01:00:39 +0530

    addition of login and signup page

--- src/notes.js
+++ src/notes.js
@@ -1,9 +1,23 @@
 function toggleSection(sectionNumber) {
-    var content = document.getElementById('section-' + sectionNumber);
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
+  var content = document.getElementById("section-" + sectionNumber);
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
 }
 
+document
+  .querySelectorAll('.file-upload input[type="file"]')
+  .forEach((input) => {
+    input.addEventListener("change", function () {
+      if (this.files && this.files[0]) {
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          document.getElementById("pdf-frame").src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  });
