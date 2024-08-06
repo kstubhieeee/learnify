38a4c18b484457c8b048bd6f7688193858ca17b5
kstubhie <banekaustubh27@gmail.com>
2024-08-07 02:45:08 +0530

    video lecs updated

--- src/lecture.js
+++ src/lecture.js
@@ -1,14 +1,26 @@
 var acc = document.getElementsByClassName("accordion");
-var i;
+var videoLinks = document.getElementsByClassName("video-link");
+var videoPlayer = document.getElementById("videoPlayer");
+var videoTitle = document.getElementById("videoTitle");
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+for (var j = 0; j < videoLinks.length; j++) {
+  videoLinks[j].addEventListener("click", function () {
+    var videoId = this.getAttribute("data-video-id");
+    var title = this.textContent;
+    videoPlayer.src =
+      "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+    videoTitle.textContent = title;
+  });
 }
